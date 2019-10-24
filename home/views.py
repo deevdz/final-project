2c495d10fb798a33fdf3eeaf83a7988dbc1f5ee7
Ubuntu <ubuntu@ip-172-31-34-233.ec2.internal>
2019-10-24 11:40:51 +0000

    Add blog posts to homepage

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Blog
 
 
 # Create your views here.
 def index(request):
     """A view that displays the Homepage"""
-    return render(request, 'index.html',{})
+    blog_posts = Blog.objects.filter(status='published').order_by('-created_date')[:3]
+    
+    context = {
+            'queryset': blog_posts,
+            }
+    
+    return render(request, 'index.html', context)
     
 def about(request):
     """A view that displays the About Page"""
