acd511f97728ea14a2f73823575b8adadb5a7c17
Ubuntu <ubuntu@ip-172-31-34-233.ec2.internal>
2020-01-29 14:24:11 +0000

    Checking Automated tests

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -103,12 +103,7 @@ WSGI_APPLICATION = 'yogastudio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    print("Database URL not found. Using SQLite instead")
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
