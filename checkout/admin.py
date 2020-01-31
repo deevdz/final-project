3752d694ef5c74fa4c145d7d223b5093dc589961
Ubuntu <ubuntu@ip-172-31-34-233.ec2.internal>
2020-01-31 15:43:37 +0000

    Fix for Admin Dashboard - Addresses

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,7 +5,12 @@ from .models import Address, Order
 # Admin - Order: What to display in the backend view
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('user', 'created', 'ordered')
+    
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'address_line_1',
+                    'address_line_2', 'town', 'county',
+                    'postcode')
 
 # Register your models here.
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Address)
+admin.site.register(Address, AddressAdmin)
